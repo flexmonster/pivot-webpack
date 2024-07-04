43d77cf3326c57f32b70b9331520bc1ab4798bc4
Volodymyr Fomin <w260698@gmail.com>
2024-07-04 12:51:20 +0300

    Added sample data

--- src/index.js
+++ src/index.js
@@ -8,5 +8,26 @@ import Flexmonster from 'flexmonster';
 
 new Flexmonster({
     container: "#pivot",
-    toolbar: true
+    toolbar: true,
+    report: {
+        dataSource: {
+            data: [
+                {
+                    "Category": "Accessories",
+                    "Price": 125,
+                    "Quantity": 100
+                },
+                {
+                    "Category": "Accessories",
+                    "Price": 74,
+                    "Quantity": 235
+                },
+                {
+                    "Category": "Bikes",
+                    "Price": 233,
+                    "Quantity": 184
+                }
+            ]
+        }
+    }
 })
